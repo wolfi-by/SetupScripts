f4c81cf75903e570505cf3f34cdb33ee763eade9
Wolfgang Höhne <wolfi-by@users.noreply.github.com>
2021-02-25 12:18:38 +0100

    Update InstallDotNet.sh

--- Raspberry/InstallDotNet.sh
+++ Raspberry/InstallDotNet.sh
@@ -4,7 +4,10 @@ wget https://download.visualstudio.microsoft.com/download/pr/cd11b0d1-8d79-493f-
 mkdir -p $HOME/dotnet && tar zxf dotnet-sdk-5.0.103-linux-arm64.tar.gz -C $HOME/dotnet
 export DOTNET_ROOT=$HOME/dotnet
 export PATH=$PATH:$HOME/dotnet
-touch ~/.bashrc
-echo "export PATH=$PATH:$HOME/dotnet" >> ~/.bashrc
-echo "export DOTNET_ROOT=$HOME/dotnet" >> ~/.bashrc
+
+cat > ~/.bashrc << EOF
+export PATH=$PATH:$HOME/dotnet
+DOTNET_ROOT=$HOME/dotnet
+EOF
+
 apt-get install -y libgdiplus libc6 libgcc1 libgssapi-krb5-2 libicu63 libssl1.1 libstdc++6 zlib1g net-tools iw hostapd dnsmasq iptables-persistent
