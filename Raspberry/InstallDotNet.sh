ff38f8ee62f47f3ce8e13652e3f746f036af8665
Wolfgang Höhne <wolfi-by@users.noreply.github.com>
2021-02-25 11:42:20 +0100

    Update and rename InstallDotNet to InstallDotNet.sh

--- Raspberry/InstallDotNet.sh
+++ Raspberry/InstallDotNet.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 cd /tmp
 
 mkdir -p $HOME/dotnet && tar zxf dotnet-sdk-5.0.103-linux-arm64.tar.gz -C $HOME/dotnet
@@ -7,3 +8,5 @@ export PATH=$PATH:$HOME/dotnet
 touch ~/.bashrc
 echo "export PATH=$PATH:$HOME/dotnet" >> ~/.bashrc
 echo "export DOTNET_ROOT=$HOME/dotnet" >> ~/.bashrc
+
+apt-get install -y libgdiplus libc6 libgcc1 libgssapi-krb5-2 libicu63 libssl1.1 libstdc++6 zlib1g net-tools iw hostapd dnsmasq iptables-persistent
