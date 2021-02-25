fbabd2401e169f15afc117f350e38af19b859ac0
Wolfgang Höhne <wolfi-by@users.noreply.github.com>
2021-02-25 12:31:58 +0100

    Update InstallDotNet6.sh

--- Raspberry/InstallDotNet6.sh
+++ Raspberry/InstallDotNet6.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /tmp
-https://download.visualstudio.microsoft.com/download/pr/3b62cfcf-589e-43b3-993b-517c70c93a22/0ecae846884376fecc5de8a4f6d6c927/dotnet-sdk-6.0.100-preview.1.21103.13-linux-arm.tar.gz
+wget https://download.visualstudio.microsoft.com/download/pr/3b62cfcf-589e-43b3-993b-517c70c93a22/0ecae846884376fecc5de8a4f6d6c927/dotnet-sdk-6.0.100-preview.1.21103.13-linux-arm.tar.gz
 mkdir -p $HOME/dotnet && tar zxf dotnet-sdk-6.0.100-preview.1.21103.13-linux-arm.tar.gz -C $HOME/dotnet
 export DOTNET_ROOT=$HOME/dotnet
 export PATH=$PATH:$HOME/dotnet
